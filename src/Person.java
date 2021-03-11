8ca745cc652966f24a3fa214ea12dfeb30ea2b0c
TheFloeffel <64588087+TheFloeffel@users.noreply.github.com>
2021-03-11 13:52:56 +0100

    Transformer

---
+++ src/Person.java
@@ -0,0 +1,24 @@
+public class Person {
+
+    private String name;
+    private int weight;
+
+    public Person(String name, int weight) {
+        this.name = name;
+        this.weight = weight;
+    }
+
+    public void talk(String text) {
+        System.out.println(text);
+    }
+
+    public void eat() {
+    }
+
+    public void fight() {
+    }
+
+    public void die() {
+    }
+
+}
